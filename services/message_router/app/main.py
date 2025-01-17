4b66440c9b71867c2efb4b2dfcae2d83e548e164
Siarhei Ryzhou <s.a.ryzhov@gmail.com>
2025-01-17 16:22:15 +0300

    Update structure to launch

---
+++ services/message_router/app/main.py
@@ -0,0 +1,6 @@
+import time
+
+if __name__ == "__main__":
+    print("Message Router is running...")
+    while True:
+        time.sleep(1)
---
+++ services/rabbitmq_worker/app/main.py
@@ -0,0 +1,6 @@
+import time
+
+if __name__ == "__main__":
+    print("RabbitMQ Worker is running...")
+    while True:
+        time.sleep(1)
